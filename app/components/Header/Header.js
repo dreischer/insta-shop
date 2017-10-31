9415a0be6d3f325976cdbc5bd85df7609aeb2414
dreischer <reischer.david@gmail.com>
2017-10-31 17:33:28 +0000

    adding state management

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -3,9 +3,13 @@ import { Link } from 'preact-router'
 import './Header.less'
 
 export default class Header extends Component {
-  render () {
+  render (props, state) {
+    const { atom } = props
+    const image = atom.header.icon ? <img class='header-icon' src={atom.header.icon} /> : null
+
     return (
       <header className='header'>
+        {image}
         <h1>Preact App</h1>
         <nav>
           <Link href='/'>Home</Link>
--- app/index.js
+++ app/index.js
@@ -2,16 +2,32 @@ import React from 'preact'
 import App from './App'
 import axios from 'axios'
 
+import createAtom from 'tiny-atom'
+
 window.Promise = window.Promise || require('es6-promise').Promise
 window.React = React
 window.axios = axios
 
-render()
-
 let root
 
+const atom = createAtom({
+  header: {
+    icon: null
+  }
+}, evolve, render)
+
+function evolve (getState, split, action) {
+  const { type, payload } = action
+
+  if (type === 'header') {
+    split({ header: payload })
+  }
+}
+
+render()
+
 function render () {
-  root = React.render(<App />, document.getElementById('root'), root)
+  root = React.render(<App atom={atom.get()} split={atom.split} />, document.getElementById('root'), root)
 }
 
 // in development, set up HMR:
